037f73828d1938ca4ec68c19a3157541f1b8d123
Julian Wulff <julianwulff@gmail.com>
2022-05-10 11:48:34 +0200

    Created intial draft for the DCRGraph interface

---
+++ app/src/core/models/DCRGraph.ts
@@ -0,0 +1,52 @@
+export interface DCRGraph {
+  activies: Activity[];
+  relations: Relation[];
+  metaData: {
+    name: string;
+    // Saved for meta data
+  };
+}
+
+export interface Activity {
+  aid: Aid;
+  label: string;
+  position: Position;
+  marking: {
+    pending: boolean;
+    included: boolean;
+    executed: boolean;
+  };
+  // Visuals
+  customization: {
+    // borderColor: string
+    // textColor: string;
+    // bgColor: string;
+    // borderType: BorderType;
+  };
+  relations: Relation[];
+  parrent: Aid;
+  nestedActivities: Activity[];
+}
+
+type Aid = string;
+enum RelationType {
+  PreCondition,
+  Response,
+  LogicalInclude,
+  NoResponse,
+  Include,
+  Exclude,
+  Spawn,
+  Condition,
+  Milestone,
+  Value
+}
+export interface Relation {
+  to: Aid;
+  type: RelationType;
+}
+
+export interface Position {
+  x: number;
+  y: number;
+}
