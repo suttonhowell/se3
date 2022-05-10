060509439f0887133d3602746871ec9f326af6e5
Julian Wulff <julianwulff@gmail.com>
2022-05-10 18:36:14 +0200

    Added support for importing static assets

--- app/webpack.plugins.js
+++ app/webpack.plugins.js
@@ -1,4 +1,16 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
 
-module.exports = [new ForkTsCheckerWebpackPlugin()];
+module.exports = [
+  new ForkTsCheckerWebpackPlugin(),
+  new CopyWebpackPlugin({
+    patterns: [
+      {
+        from: path.resolve(__dirname, 'src/assets/'),
+        to: path.resolve(__dirname, '.webpack/renderer/assets/'),
+      },
+    ],
+  }),
+];
