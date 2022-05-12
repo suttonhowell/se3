57c5a96daf8fb607ee18da807fd5474e5328c8d0
Julian Wulff <julianwulff@gmail.com>
2022-05-12 11:49:07 +0200

    Added the Add activity button

---
+++ app/src/pages/Editor/components/AddActivity.tsx
@@ -0,0 +1,19 @@
+import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
+import { Fab } from '@mui/material';
+
+export const AddActivity = () => {
+  const handleOnClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent> | undefined) => {
+    console.log('Fab was clicked', e?.clientX, e?.clientY);
+  };
+
+  return (
+    <Fab
+      color="primary"
+      onClick={(e) => handleOnClick(e)}
+      aria-label="add"
+      sx={{ position: 'absolute', bottom: 16, left: 16 }}
+    >
+      <AddOutlinedIcon />
+    </Fab>
+  );
+};
