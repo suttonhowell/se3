7441b09fb7483e25ef7f29f5e05302958b74b0d0
Julian Wulff <julianwulff@gmail.com>
2022-05-13 13:04:41 +0200

    Fix font and bg colors for dark/liht mode

--- app/src/components/layout/AppContainer.tsx
+++ app/src/components/layout/AppContainer.tsx
@@ -1,15 +1,16 @@
-import { Box } from '@mui/material';
+import { Box, Paper } from '@mui/material';
 import { Outlet } from 'react-router-dom';
 import { TopBar } from './TopBar';
 
 export const AppContainer = () => {
   return (
-    <Box sx={{ display: 'flex', height: '100%', flexDirection: 'column', bgColor: 'background' }}>
+    <Box
+      component={Paper}
+      sx={{ display: 'flex', height: '100%', flexDirection: 'column' }}
+      elevation={2}
+    >
       <TopBar />
-      <Box
-        component="main"
-        sx={{ flexGrow: 1, bgcolor: 'background.default', mt: 8, height: '100%' }}
-      >
+      <Box component="main" sx={{ flexGrow: 1, mt: 8, height: '100%' }}>
         <Outlet />
       </Box>
     </Box>
