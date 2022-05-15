0cc5959520e8931f00d9c6d0525c62d30d07055f
Daniel Ellebæk Nielsen <daniogdan@gmail.com>
2022-05-15 22:32:59 +0200

    Add button functionality

--- app/src/pages/Editor/components/TopToolbar.tsx
+++ app/src/pages/Editor/components/TopToolbar.tsx
@@ -21,7 +21,8 @@ import { useState } from 'react';
 const zoomList = [25, 50, 75, 100, 150, 200];
 
 export const TopToolbar = () => {
-  const [hasHistory, setHasHistory] = useState<boolean>(false);
+  const [hasHistory, setHasHistory] = useState<boolean>(true);
+  const [hasSelected, setHasSelected] = useState<boolean>(true);
   const [zoomAnchor, setAnchorZoom] = useState<null | HTMLElement>(null);
   const [zoomLevel, setZoomLevel] = useState(100);
   const zoomOpen = Boolean(zoomAnchor);
@@ -35,26 +36,32 @@ export const TopToolbar = () => {
 
   const handleOnClickUndo = () => {
     console.log('Undo!');
+    setHasHistory((prev) => !prev);
   };
 
   const handleOnClickRedo = () => {
     console.log('Redo!');
+    setHasHistory((prev) => !prev);
   };
 
   const handleOnClickHistory = () => {
     console.log('History!');
+    setHasHistory((prev) => !prev);
   };
 
   const handleOnClickZoomIn = () => {
     console.log('Zoom in!');
+    setZoomLevel((prev) => zoomList[zoomList.indexOf(prev) + 1]);
   };
 
   const handleOnClickZoomOut = () => {
     console.log('Zoom out!');
+    setZoomLevel((prev) => zoomList[zoomList.indexOf(prev) - 1]);
   };
 
   const handleOnClickDelete = () => {
     console.log('Delete!');
+    setHasSelected((prev) => !prev);
   };
 
   return (
@@ -64,25 +71,25 @@ export const TopToolbar = () => {
           <Box sx={{ display: 'flex' }}>
             <Box sx={{ px: 1 }}>
               <Tooltip title="Undo" arrow disableInteractive>
-                <IconButton onClick={handleOnClickUndo} aria-label="undo">
+                <IconButton disabled={!hasHistory} onClick={handleOnClickUndo} aria-label="undo">
                   <UndoRoundedIcon />
                 </IconButton>
               </Tooltip>
 
               <Tooltip title="Redo" arrow disableInteractive>
-                <IconButton onClick={handleOnClickRedo} aria-label="redo">
+                <IconButton disabled={!hasHistory} onClick={handleOnClickRedo} aria-label="redo">
                   <RedoRoundedIcon />
                 </IconButton>
               </Tooltip>
 
               <Tooltip title="History" arrow disableInteractive>
-                <IconButton onClick={handleOnClickHistory} aria-label="history">
+                <IconButton disabled={!hasHistory} onClick={handleOnClickHistory} aria-label="history">
                   <HistoryRoundedIcon />
                 </IconButton>
               </Tooltip>
             </Box>
             <Divider orientation="vertical" flexItem sx={{ borderRightWidth: 2 }} />
-            <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', px: 2 }}>
+            <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', px: 2, width: '80px' }}>
               <Tooltip title="Zoom" arrow disableInteractive>
                 <Button onClick={handleOnClickZoom} endIcon={<KeyboardArrowDown />}>
                   {zoomLevel}%
@@ -92,13 +99,13 @@ export const TopToolbar = () => {
             <Divider orientation="vertical" flexItem sx={{ borderRightWidth: 2 }} />
             <Box sx={{ px: 1 }}>
               <Tooltip title="Zoom in" arrow disableInteractive>
-                <IconButton onClick={handleOnClickZoomIn} aria-label="zoomin">
+                <IconButton disabled={zoomList.indexOf(zoomLevel) === zoomList.length - 1} onClick={handleOnClickZoomIn} aria-label="zoomin">
                   <ZoomInRoundedIcon />
                 </IconButton>
               </Tooltip>
 
               <Tooltip title="Zoom out" arrow disableInteractive>
-                <IconButton onClick={handleOnClickZoomOut} aria-label="zoomout">
+                <IconButton disabled={zoomList.indexOf(zoomLevel) === 0} onClick={handleOnClickZoomOut} aria-label="zoomout">
                   <ZoomOutRoundedIcon />
                 </IconButton>
               </Tooltip>
@@ -106,7 +113,7 @@ export const TopToolbar = () => {
             <Divider orientation="vertical" flexItem sx={{ borderRightWidth: 2 }} />
             <Box sx={{ px: 1 }}>
               <Tooltip title="Delete" arrow disableInteractive>
-                <IconButton onClick={handleOnClickDelete} aria-label="delete">
+                <IconButton disabled={!hasSelected} onClick={handleOnClickDelete} aria-label="delete">
                   <DeleteRoundedIcon />
                 </IconButton>
               </Tooltip>
