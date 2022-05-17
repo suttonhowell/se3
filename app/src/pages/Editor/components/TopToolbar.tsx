a2a48c202c2d1541b9aaf5c49fb15860bdb48cc7
Julian Wulff <julianwulff@gmail.com>
2022-05-17 16:17:36 +0200

    Refactor TopToolbar - reduce code duplication and use dropdown component

--- app/src/pages/Editor/components/TopToolbar.tsx
+++ app/src/pages/Editor/components/TopToolbar.tsx
@@ -1,165 +1,182 @@
-import { KeyboardArrowDown } from '@mui/icons-material';
-import DeleteRoundedIcon from '@mui/icons-material/DeleteRounded';
-import HistoryRoundedIcon from '@mui/icons-material/HistoryRounded';
-import RedoRoundedIcon from '@mui/icons-material/RedoRounded';
-import UndoRoundedIcon from '@mui/icons-material/UndoRounded';
-import ZoomInRoundedIcon from '@mui/icons-material/ZoomInRounded';
-import ZoomOutRoundedIcon from '@mui/icons-material/ZoomOutRounded';
 import {
-  AppBar,
-  Button,
-  Divider,
-  IconButton,
-  Menu,
-  MenuItem,
-  Toolbar,
-  Tooltip,
-} from '@mui/material';
+  DeleteRounded as DeleteIcon,
+  HistoryRounded as HistoryIcon,
+  RedoRounded as RedoIcon,
+  UndoRounded as UndoIcon,
+  ZoomInRounded as ZoomInIcon,
+  ZoomOutRounded as ZoomOutIcon,
+} from '@mui/icons-material';
+import { AppBar, Divider, IconButton, Toolbar, Tooltip } from '@mui/material';
 import { Box } from '@mui/system';
-import { useState } from 'react';
+import React, { Fragment, useState } from 'react';
+import { ButtonDropDown, DropDownItemProps } from '../../../components/navigation/ButtonDropDown';
 
-const zoomList = [25, 50, 75, 100, 150, 200];
+type MenuState = {
+  hasHistory: boolean;
+  hasSelected: boolean;
+  zoomLevel: number;
+};
 
 export const TopToolbar = () => {
-  const [hasHistory, setHasHistory] = useState<boolean>(true);
-  const [hasSelected, setHasSelected] = useState<boolean>(true);
-  const [zoomAnchor, setAnchorZoom] = useState<null | HTMLElement>(null);
-  const [zoomLevel, setZoomLevel] = useState(100);
-  const zoomOpen = Boolean(zoomAnchor);
-  const handleOnClickZoom = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorZoom(event.currentTarget);
-  };
-  const zoomHandleClose = (level: number) => {
-    setZoomLevel(level);
-    setAnchorZoom(null);
+  const [menuState, setMenuState] = useState<MenuState>({
+    hasHistory: true,
+    hasSelected: true,
+    zoomLevel: 100,
+  });
+
+  const handleOnClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    const { name } = e.currentTarget;
+    switch (name) {
+      case 'undo':
+      case 'redo':
+      case 'history':
+        setMenuState((prevState) => ({
+          ...prevState,
+          hasHistory: !prevState.hasHistory,
+        }));
+        break;
+      case 'zoomIn':
+        setMenuState((prevState) => ({
+          ...prevState,
+          zoomLevel: prevState.zoomLevel + 25,
+        }));
+        break;
+      case 'zoomIn':
+        setMenuState((prevState) => ({
+          ...prevState,
+          zoomLevel: prevState.zoomLevel + 25,
+        }));
+        break;
+      case 'zoomOut':
+        setMenuState((prevState) => ({
+          ...prevState,
+          zoomLevel: prevState.zoomLevel - 15,
+        }));
+        break;
+      case 'delete':
+        setMenuState((prevState) => ({
+          ...prevState,
+          hasSelected: !prevState.hasSelected,
+        }));
+        break;
+      default:
+        break;
+    }
   };
 
-  const handleOnClickUndo = () => {
-    console.log('Undo!');
-    setHasHistory((prev) => !prev);
-  };
-
-  const handleOnClickRedo = () => {
-    console.log('Redo!');
-    setHasHistory((prev) => !prev);
-  };
-
-  const handleOnClickHistory = () => {
-    console.log('History!');
-    setHasHistory((prev) => !prev);
-  };
-
-  const handleOnClickZoomIn = () => {
-    console.log('Zoom in!');
-    setZoomLevel((prev) => zoomList[zoomList.indexOf(prev) + 1]);
-  };
-
-  const handleOnClickZoomOut = () => {
-    console.log('Zoom out!');
-    setZoomLevel((prev) => zoomList[zoomList.indexOf(prev) - 1]);
-  };
-
-  const handleOnClickDelete = () => {
-    console.log('Delete!');
-    setHasSelected((prev) => !prev);
+  const handleOnZoomLevelClick = (e: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
+    const { value } = e.currentTarget;
+    setMenuState((prevState) => ({
+      ...prevState,
+      zoomLevel: value,
+    }));
   };
 
   return (
     <>
       <AppBar position="relative" color="transparent" sx={{ bgcolor: 'grey.100' }}>
-        <Toolbar disableGutters>
-          <Box sx={{ display: 'flex' }}>
-            <Box sx={{ px: 1 }}>
-              <Tooltip title="Undo" arrow disableInteractive>
-                <IconButton disabled={!hasHistory} onClick={handleOnClickUndo} aria-label="undo">
-                  <UndoRoundedIcon />
-                </IconButton>
-              </Tooltip>
-
-              <Tooltip title="Redo" arrow disableInteractive>
-                <IconButton disabled={!hasHistory} onClick={handleOnClickRedo} aria-label="redo">
-                  <RedoRoundedIcon />
-                </IconButton>
-              </Tooltip>
-
-              <Tooltip title="History" arrow disableInteractive>
-                <IconButton
-                  disabled={!hasHistory}
-                  onClick={handleOnClickHistory}
-                  aria-label="history"
-                >
-                  <HistoryRoundedIcon />
-                </IconButton>
-              </Tooltip>
-            </Box>
-            <Divider orientation="vertical" flexItem sx={{ borderRightWidth: 2 }} />
-            <Box
-              sx={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                px: 2,
-                width: '80px',
-              }}
-            >
-              <Tooltip title="Zoom" arrow disableInteractive>
-                <Button onClick={handleOnClickZoom} endIcon={<KeyboardArrowDown />}>
-                  {zoomLevel}%
-                </Button>
-              </Tooltip>
-            </Box>
-            <Divider orientation="vertical" flexItem sx={{ borderRightWidth: 2 }} />
-            <Box sx={{ px: 1 }}>
-              <Tooltip title="Zoom in" arrow disableInteractive>
-                <IconButton
-                  disabled={zoomList.indexOf(zoomLevel) === zoomList.length - 1}
-                  onClick={handleOnClickZoomIn}
-                  aria-label="zoomin"
-                >
-                  <ZoomInRoundedIcon />
-                </IconButton>
-              </Tooltip>
-
-              <Tooltip title="Zoom out" arrow disableInteractive>
-                <IconButton
-                  disabled={zoomList.indexOf(zoomLevel) === 0}
-                  onClick={handleOnClickZoomOut}
-                  aria-label="zoomout"
-                >
-                  <ZoomOutRoundedIcon />
-                </IconButton>
-              </Tooltip>
-            </Box>
-            <Divider orientation="vertical" flexItem sx={{ borderRightWidth: 2 }} />
-            <Box sx={{ px: 1 }}>
-              <Tooltip title="Delete" arrow disableInteractive>
-                <IconButton
-                  disabled={!hasSelected}
-                  onClick={handleOnClickDelete}
-                  aria-label="delete"
-                >
-                  <DeleteRoundedIcon />
-                </IconButton>
-              </Tooltip>
-            </Box>
-          </Box>
+        <Toolbar disableGutters variant="dense" sx={{ minHeight: 'unset', py: 0.5 }}>
+          {menuItemButtonGroups.map((buttonGroup, idx) => (
+            <Fragment key={'btnGrp' + idx}>
+              <Box sx={{ px: 1 }}>
+                {buttonGroup.map((button) =>
+                  button.icon ? (
+                    <Tooltip key={button.name} title={button.toolTipTitle} arrow disableInteractive>
+                      <IconButton
+                        disabled={!menuState[button.state] ? true : false}
+                        name={button.name}
+                        onClick={handleOnClick}
+                      >
+                        {button.icon}
+                      </IconButton>
+                    </Tooltip>
+                  ) : (
+                    <ButtonDropDown
+                      key={button.name}
+                      tooltipTitle={button.toolTipTitle}
+                      buttonText={menuState.zoomLevel + '%'}
+                      menuItems={zoomItemList}
+                      onClickMenuItem={handleOnZoomLevelClick}
+                    />
+                  )
+                )}
+              </Box>
+              {idx !== menuItemButtonGroups.length - 1 && (
+                <Divider orientation="vertical" flexItem sx={{ borderRightWidth: 2, my: 1 }} />
+              )}
+            </Fragment>
+          ))}
         </Toolbar>
       </AppBar>
-      <Menu
-        id="basic-menu"
-        anchorEl={zoomAnchor}
-        open={zoomOpen}
-        onClose={zoomHandleClose}
-        MenuListProps={{
-          'aria-labelledby': 'basic-button',
-        }}
-      >
-        {zoomList.map((item, idx) => (
-          <MenuItem key={item + idx} onClick={(_) => zoomHandleClose(item)}>
-            {item}
-          </MenuItem>
-        ))}
-      </Menu>
     </>
   );
 };
+
+const zoomList = [25, 50, 75, 100, 150, 200];
+const zoomItemList: DropDownItemProps[] = zoomList.map((item) => ({
+  label: item.toString() + '%',
+  value: item,
+  name: 'zoomLevel',
+  key: item,
+}));
+
+interface MenuButtonProps {
+  name: string;
+  toolTipTitle: string;
+  icon?: JSX.Element;
+  state: keyof MenuState;
+}
+
+type MenuItemButtonGroupProps = MenuButtonProps[];
+
+const menuItemButtonGroups: MenuItemButtonGroupProps[] = [
+  [
+    {
+      name: 'undo',
+      toolTipTitle: 'Undo',
+      icon: <UndoIcon />,
+      state: 'hasHistory',
+    },
+    {
+      name: 'redo',
+      toolTipTitle: 'Redo',
+      icon: <RedoIcon />,
+      state: 'hasHistory',
+    },
+    {
+      name: 'history',
+      toolTipTitle: 'See change history',
+      icon: <HistoryIcon />,
+      state: 'hasHistory',
+    },
+  ],
+  [
+    {
+      name: 'zoomLevel',
+      toolTipTitle: 'Zoom',
+      state: 'zoomLevel',
+    },
+  ],
+  [
+    {
+      name: 'zoomIn',
+      toolTipTitle: 'Zoom in',
+      icon: <ZoomInIcon />,
+      state: 'zoomLevel',
+    },
+    {
+      name: 'zoomOut',
+      toolTipTitle: 'Zoom out',
+      icon: <ZoomOutIcon />,
+      state: 'zoomLevel',
+    },
+  ],
+  [
+    {
+      name: 'delete',
+      toolTipTitle: 'Delete',
+      icon: <DeleteIcon />,
+      state: 'hasSelected',
+    },
+  ],
+];
