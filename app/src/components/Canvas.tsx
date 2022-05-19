3dc426a34adc6b53c4b6ccfb57739969b801ec4b
biekin <26335041+biekin@users.noreply.github.com>
2022-05-19 13:55:21 +0200

    Revert "Create top toolbar"

--- app/src/components/Canvas.tsx
+++ app/src/components/Canvas.tsx
@@ -26,6 +26,9 @@ export const Canvas = () => {
         style={{
           height: '100%',
           width: '100%',
+          // position: 'absolute',
+          top: 0,
+          left: 0
         }}
       ></svg>
     </>
