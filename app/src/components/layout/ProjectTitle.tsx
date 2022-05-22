be98899f880a592812a2a84df0f022b30c12612b
Isabella <rvm681@ku.dk>
2022-05-22 20:22:15 +0200

    Add editable title and clickable pencil
    with react states

--- app/src/components/layout/ProjectTitle.tsx
+++ app/src/components/layout/ProjectTitle.tsx
@@ -1,18 +1,57 @@
-import { Edit } from "@mui/icons-material";
-import { Box, Typography } from "@mui/material";
-import TextField from "@mui/material/TextField";
+import { Edit } from '@mui/icons-material';
+import { Box, IconButton, TextField } from '@mui/material';
+import React, { useState } from 'react';
 
 export const ProjectTitle = () => {
-    // since we don't want to have the title in the frontpage
-    let isbeingEdited = true; //TODO set based on the pencil icon click
-    let title = "Untitled"; //TODO sync this with redux 
+  const [title, setTitle] = useState<string>('Title');
+  const [isTitleEditable, setIsTitleEditable] = useState<boolean>(true);
+  const [isError, setIsError] = useState<boolean>(false);
+  const titleMaxLength = 20;
 
-    return (
-        <Box>
-            {isbeingEdited
-            ? <TextField  label={title} variant="standard" />
-            : <Typography> { title } </Typography> }
-        <Edit/>
-        </Box>
-    )
-}
+  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsTitleEditable(() => true); // Enable pencil icon upon title change
+  };
+
+  const updateTitleorErrorMsg = () => {
+    var titleFieldValue = (document.getElementById('TitleField') as HTMLInputElement).value;
+    console.log('New title: ', titleFieldValue);
+    if (titleFieldValue.length >= titleMaxLength) {
+      setIsError(() => true);
+    } else {
+      setIsError(() => false); // Reset after potential error
+      setTitle(titleFieldValue);
+      // Disable pencil icon so the same name cannot be saved multiple times
+      setIsTitleEditable(() => false);
+    }
+  };
+
+  const handleKeyDownInTitle = (event: any) => {
+    if (event.keyCode === 13) {
+      console.log('ENTER!');
+      updateTitleorErrorMsg();
+    }
+  };
+
+  const handleOnClickPencil = () => {
+    console.log('PENCIL');
+    updateTitleorErrorMsg();
+  };
+
+  return (
+    <Box>
+      <TextField
+        id="TitleField"
+        error={isError}
+        helperText={isError ? 'Please enter a title with less than 20 characters' : ''}
+        label={title}
+        defaultValue="Untitled"
+        variant="standard"
+        onChange={handleTitleChange}
+        onKeyDown={handleKeyDownInTitle}
+      />
+      <IconButton disabled={!isTitleEditable}>
+        <Edit onClick={handleOnClickPencil} />
+      </IconButton>
+    </Box>
+  );
+};
--- app/src/components/layout/TopBar.tsx
+++ app/src/components/layout/TopBar.tsx
@@ -4,7 +4,7 @@ import { ProjectTitle } from './ProjectTitle';
 
 export const TopBar = () => {
   // since we don't want to have the title in the frontpage
-  let isInEditor = false; // TODO get this info from router
+  let isInEditor = true; // TODO get this info from router
 
   return (
     <AppBar
@@ -25,12 +25,14 @@ export const TopBar = () => {
           }}
         >
           <AccountTreeIcon fontSize="medium" />
-        </Box>   
-        {isInEditor 
-        ? <ProjectTitle/>
-        : <Typography variant="h6" noWrap component="div">
-          DCR Graph Editor
-        </Typography>}  
+        </Box>
+        {isInEditor ? (
+          <ProjectTitle />
+        ) : (
+          <Typography variant="h6" noWrap component="div">
+            DCR Graph Editor
+          </Typography>
+        )}
       </Toolbar>
     </AppBar>
   );
