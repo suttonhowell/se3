d1fc3e86e79b9aafeeb4568a74ad5498ea5348aa
biekin <26335041+biekin@users.noreply.github.com>
2022-05-22 15:58:48 +0200

    Add edit button

--- app/src/components/layout/ProjectTitle.tsx
+++ app/src/components/layout/ProjectTitle.tsx
@@ -1,17 +1,18 @@
-import { Typography } from "@mui/material";
+import { Edit } from "@mui/icons-material";
+import { Box, Typography } from "@mui/material";
 import TextField from "@mui/material/TextField";
 
 export const ProjectTitle = () => {
     // since we don't want to have the title in the frontpage
     let isbeingEdited = true; //TODO set based on the pencil icon click
     let title = "Untitled"; //TODO sync this with redux 
-    if(isbeingEdited){
-        return (
-        <TextField  label={title} variant="standard" />
-    )
-    }
-    else
+
     return (
-        <Typography> { title } </Typography>
-    )  
+        <Box>
+            {isbeingEdited
+            ? <TextField  label={title} variant="standard" />
+            : <Typography> { title } </Typography> }
+        <Edit/>
+        </Box>
+    )
 }
