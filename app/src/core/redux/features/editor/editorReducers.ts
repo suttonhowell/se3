0fbb01c7e8203dba8c524a6111bad6d892c71f76
Julian Wulff <julianwulff@gmail.com>
2022-06-01 00:45:25 +0200

    Add addRelation and pickTool to editorSlicer

---
+++ app/src/core/redux/features/editor/editorReducers.ts
@@ -0,0 +1,46 @@
+// ! This file is meant container large reducers to make the slice more readable
+import { PayloadAction } from '@reduxjs/toolkit';
+import { WritableDraft } from 'immer/dist/internal';
+import { Aid } from '../../../../core/models';
+import {
+  createNewRelationsToSelf,
+  createNewRelationToOther,
+} from '../../../../core/models/Relations';
+import { EditorState } from './editorSlice';
+
+export const addRelationReducer = (
+  state: WritableDraft<EditorState>,
+  action: PayloadAction<Aid | null>
+) => {
+  // Use null as payload to cancel addRelation
+  if (!action.payload) {
+    return { ...state, addRelationArgs: null };
+  }
+  const addRelationArgs = state.addRelationArgs;
+  // No from args has been selected thus the payload is the "from" activity
+  if (!addRelationArgs) {
+    return { ...state, addRelationArgs: action.payload };
+  }
+  // Is Relation to self
+  if (addRelationArgs === action.payload) {
+    state.addRelationArgs = null;
+    const updatedActivities = state.graph.activies.map((a) =>
+      a.aid !== addRelationArgs
+        ? a
+        : { ...a, relations: [...a.relations, createNewRelationsToSelf(a.aid)] }
+    );
+    state.graph.activies = updatedActivities;
+  } else {
+    // Is Relation to other
+    return {
+      ...state,
+      graph: {
+        ...state.graph,
+        relations: [
+          ...state.graph.relations,
+          createNewRelationToOther(addRelationArgs, action.payload),
+        ],
+      },
+    };
+  }
+};
--- app/src/core/redux/features/editor/editorSlice.ts
+++ app/src/core/redux/features/editor/editorSlice.ts
@@ -1,16 +1,26 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { Aid, DCRGraph, initialActivity, initialDCRGraph, Position } from '../../../../core/models';
+import { addRelationReducer } from './editorReducers';
 
-interface EditorState {
+export enum ToolType {
+  None = 'None',
+  AddRelation = 'AddRelation',
+}
+
+export interface EditorState {
   graph: DCRGraph;
   selectedElement: Aid | null;
   offset: Position | null;
+  usingTool: ToolType;
+  addRelationArgs: Aid | null;
 }
 
 const initialState: EditorState = {
   graph: initialDCRGraph,
   selectedElement: null,
   offset: null,
+  usingTool: ToolType.None,
+  addRelationArgs: null,
 };
 
 export const editorSlice = createSlice({
@@ -30,24 +40,36 @@ export const editorSlice = createSlice({
       state.offset = action.payload;
     },
     changeTitle: (state, action: PayloadAction<string>) => {
-      if (state.graph) {
-        state.graph.metaData.name = action.payload;
-      }
+      state.graph.metaData.name = action.payload;
+    },
+    pickTool: (state, action: PayloadAction<ToolType>) => {
+      state.usingTool = action.payload;
+      // Deselects element when a special tool is picked
+      if (action.payload !== ToolType.None) state.selectedElement = null;
+      // Removes the addRelationArg if tool deactivated early
+      if (action.payload !== ToolType.AddRelation) state.addRelationArgs = null;
     },
     moveActivity: (state, action: PayloadAction<{ aid: Aid; position: Position }>) => {
-      if (state.graph) {
-        const updatedActivities = state.graph.activies.map((activity) =>
-          activity.aid !== action.payload.aid
-            ? activity
-            : { ...activity, position: action.payload.position }
-        );
-        state.graph.activies = updatedActivities;
-      }
+      const updatedActivities = state.graph.activies.map((activity) =>
+        activity.aid !== action.payload.aid
+          ? activity
+          : { ...activity, position: action.payload.position }
+      );
+      state.graph.activies = updatedActivities;
     },
+    addRelation: addRelationReducer,
   },
 });
 
-export const { createNewGraph, addActivity, selectElement, setOffset, changeTitle, moveActivity } =
-  editorSlice.actions;
+export const {
+  addRelation,
+  createNewGraph,
+  addActivity,
+  selectElement,
+  setOffset,
+  changeTitle,
+  moveActivity,
+  pickTool,
+} = editorSlice.actions;
 
 export default editorSlice.reducer;
