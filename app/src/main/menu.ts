8801a41223b353744b87edf85c6954675954810a
Julian Wulff <julianwulff@gmail.com>
2022-06-01 22:38:07 +0200

    Move main process files into main folder, add about menu and develop menu

---
+++ app/src/main/menu.ts
@@ -0,0 +1,109 @@
+import { app } from 'electron';
+
+const isMac = process.platform === 'darwin';
+const isDebug = process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true';
+
+app.setName('DCRGraphMaker');
+export const menuTemplate: (Electron.MenuItemConstructorOptions | Electron.MenuItem)[] = [
+  ...(isMac
+    ? ([
+        {
+          label: app.name,
+          submenu: [
+            { role: 'about' },
+            { type: 'separator' },
+            { role: 'services' },
+            { type: 'separator' },
+            { role: 'hide' },
+            { role: 'hideOthers' },
+            { role: 'unhide' },
+            { type: 'separator' },
+            { role: 'quit' },
+          ],
+        },
+      ] as Electron.MenuItemConstructorOptions[])
+    : []),
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'Open',
+        click() {
+          console.log('Open was clicked\n');
+        },
+      }, //end of Open
+      {
+        label: 'Save',
+        click() {
+          console.log('Save was clicked\n');
+        },
+      }, //end of Save
+      {
+        label: 'Exit',
+        click() {
+          console.log('Exit was clicked\nExiting...\n');
+          app.quit();
+        },
+      }, //end of Exit
+    ],
+  }, //end of File submenu
+  {
+    label: 'Edit',
+    submenu: [
+      {
+        label: 'Undo',
+        click() {
+          console.log('Undo was clicked\n');
+        },
+      }, //end of Undo
+      {
+        label: 'Redo',
+        click() {
+          console.log('Redo was clicked\n');
+        },
+      }, //end of Redo
+    ],
+  }, //end of Edit submenu
+  {
+    label: 'Format',
+    submenu: [
+      {
+        label: 'Option 1',
+        click() {
+          console.log('Option 1 (Format) was clicked\n');
+        },
+      }, //end of Option 1
+      {
+        label: 'Option 2',
+        click() {
+          console.log('Option 2 (Format) was clicked\n');
+        },
+      }, //end of Option 2
+    ],
+  }, //end of Format submenu
+  {
+    label: 'Settings',
+    submenu: [
+      {
+        label: 'Option 1',
+        click() {
+          console.log('Option 1 (Settings) was clicked\n');
+        },
+      }, //end of Option 1
+      {
+        label: 'Option 2',
+        click() {
+          console.log('Option 2 (Settings) was clicked\n');
+        },
+      }, //end of Option 2
+    ],
+  }, //end of Settings submenu
+  ...(isDebug
+    ? ([
+        {
+          label: 'Development',
+          submenu: [{ role: 'reload' }, { role: 'forceReload' }, { role: 'toggleDevTools' }],
+        },
+      ] as Electron.MenuItemConstructorOptions[])
+    : []),
+];
