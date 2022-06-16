44bb80e95dda6bbc93628b9b8d6b4e9204a93693
Julian Wulff <julianwulff@gmail.com>
2022-06-16 14:23:07 +0200

    Add better responsiveness to frontpage

--- app/src/pages/Frontpage/Frontpage.tsx
+++ app/src/pages/Frontpage/Frontpage.tsx
@@ -1,12 +1,19 @@
 import { Box, Container, Grid, Typography } from '@mui/material';
+import { FrontpageImage } from './components/FronPageImage';
 import { RecentMenu } from './components/RecentMenu';
 import { StartMenu } from './components/StartMenu';
 
 const Frontpage = () => {
   return (
-    <Container maxWidth="md" sx={{ height: '100%' }}>
-      <Grid container spacing={2} alignItems="center" sx={{ minHeight: '100%' }}>
-        <Grid item xs={12} sm={6} md={6}>
+    <Container maxWidth="md" sx={{ height: '100%', px: 3 }}>
+      <Grid
+        container
+        spacing={2}
+        alignItems="center"
+        justifyContent="center"
+        sx={{ minHeight: '100%' }}
+      >
+        <Grid item xs={12} sm={8} md={6}>
           <Box sx={{ mb: 5 }}>
             <Typography variant="h1" color="textPrimary" sx={{ mb: 0.5 }}>
               DCR Maker
@@ -18,8 +25,10 @@ const Frontpage = () => {
           <StartMenu />
           <RecentMenu />
         </Grid>
-        <Grid item xs={12} sm={6} md={6}>
-          <img src="/assets/frontpage.svg" width="100%" />
+        <Grid item xs={false} sm={false} md={6}>
+          <Box display={{ xs: 'none', md: 'block' }}>
+            <FrontpageImage />
+          </Box>
         </Grid>
       </Grid>
     </Container>
